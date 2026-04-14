6a1b295d4d73bea566799715df9a0ec7e5403b45
niceguz <shawnlee2001@naver.com>
2026-04-14 20:48:06 +0900

    Create Game.cs

---
+++ GameLauncher/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GameLauncher
+{
+    public class Game
+    {
+        public string Name { get; set; } //게임 이름
+        public string Path { get; set; } //게임 실행 파일 경로
+    }
+}
